dcdfb718104890af811c4638f57678a3dae95a79
JViggiani <josh.viggiani@blueyonder.co.uk>
2018-07-24 19:04:18 +0100

    Working on foci front end

--- WebApplication1/Pages/CharacterSheetScripts.js
+++ WebApplication1/Pages/CharacterSheetScripts.js
@@ -1,13 +1,13 @@
 ﻿function partialClass() { //trying to get a partial class option list to display when "Adventurer" is selected... WiP
     // Get the text box
-    var classBox = CharacterSheet.getElementById("Class");
+    var classBox = CharacterSheet.getElementById("Class").value;
     // Get the output text
-    var partialClassBox = CharacterSheet.getElementById("PartialClass");
+    var partialClassBox = CharacterSheet.getElementById("PartialClass").value;
 
     // If the checkbox is checked, display the output text
     if (classBox === "Adventurer") {
-        partialClass.style = "visible";
+        CharacterSheet.getElementById("PartialClass").style.visibility = "visible";
     } else {
-        partialClass.style = "hidden";
+        CharacterSheet.getElementById("PartialClass").style.visibility = "hidden";
     }
 }
