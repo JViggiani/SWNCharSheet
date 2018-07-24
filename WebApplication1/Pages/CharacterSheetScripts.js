5b6d783a05acaa75e00553523b7cd524d8fc5725
JViggiani <josh.viggiani@blueyonder.co.uk>
2018-07-24 10:53:49 +0100

    Added javascripts

---
+++ WebApplication1/Pages/CharacterSheetScripts.js
@@ -0,0 +1,13 @@
+﻿function partialClass() { //trying to get a partial class option list to display when "Adventurer" is selected... WiP
+    // Get the text box
+    var classBox = CharacterSheet.getElementById("Class");
+    // Get the output text
+    var partialClassBox = CharacterSheet.getElementById("PartialClass");
+
+    // If the checkbox is checked, display the output text
+    if (classBox === "Adventurer") {
+        partialClass.style = "visible";
+    } else {
+        partialClass.style = "hidden";
+    }
+}
